5ea49f49624f4c0b390b4eacc8bf64114672068c
sunil gautam <gautams@tcd.ie>
2016-02-10 13:18:32 +0000

    got it working
    gives output now

--- pml/gui/gui.py
+++ pml/gui/gui.py
@@ -1,5 +1,6 @@
 import json
 import os
+import subprocess
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -14,11 +15,25 @@ def main():
 
 @app.route("/", methods=['POST'])
 def text2File():
-    path = os.path.join(BUCKET_PATH, "test.pml")
-    file_handle = open(path, "w+")
+
     text = request.form['text']
+    path = os.path.join(BUCKET_PATH, "test.pml")
+    file_handle = open(path, "w")
+    #save input file to local temp file
     file_handle.write(text)
     file_handle.close()
+
+    try:
+        process = subprocess.Popen(["C:/Users/sunil/Desktop/cs4098F/pml/check/pmlcheck",path],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    except OSError as error:
+        return error
+
+    output_res, err = process.communicate()
+    if process.returncode > 0:
+        return err
+    return output_res
+
 
 if __name__ == "__main__":
     app.debug = True
