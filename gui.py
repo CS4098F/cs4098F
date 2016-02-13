71c776631ae3acd0693f19a717e3b51725d20321
sunil gautam <gautams@tcd.ie>
2016-02-13 21:34:42 +0000

    clean up code
    ace works

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 import json
-import os
+import os, sys
 import subprocess
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.config['BUCKET_PATH'] = BUCKET_PATH
 
 
-@app.route("/")
+@app.route('/')
 def main():
 
     return render_template('index.html')
@@ -28,17 +28,30 @@ def uploaded_file(filename):
 def upload_file():
     if request.method == 'POST':
         files = request.files['file']
-        out_data = files.read()
-        filename = files.filename
-        path = os.path.join(BUCKET_PATH, filename)
+        if files:
+            out_data = files.read()
+            filename = files.filename
+            path = os.path.join(BUCKET_PATH, filename)
 
-        file_handle = open(path, "w")
-        #write data to file
-        file_handle.write(out_data)
-        file_handle.close()
+            file_handle = open(path, "w")
+            #write data to file
+            file_handle.write(out_data)
+            file_handle.close()
 
-        return render_template("output.html", output = out_data)
+            return render_template("editor.html", output = out_data)
+        else:
+            return redirect('/')
 
+
+# @app.route('/editor', methods=['GET', 'POST'])
+# def edit_file():
+#     if request.method == 'POST':
+#         files = request.form["file"]
+#         filename = files.filename
+#         path = os.path.join(BUCKET_PATH, filename)
+#         file_handle = open(path, "r")
+#
+#     return render_template("editor.html", output = file_handle.read() )
 
 
 @app.route("/", methods=['POST'])
@@ -51,7 +64,7 @@ def text2File():
     file_handle.write(text)
     file_handle.close()
 
-                                   #have to fix this
+
     process = subprocess.Popen(["peos/pml/check/pmlcheck",path],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     #except OSError as error:
---
+++ templates/ace/gui.py
@@ -0,0 +1,81 @@
+import json
+import os, sys
+import subprocess
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+
+
+
+CURRENT_DIRECTORY = os.getcwd()
+BUCKET_NAME = "temp_folder"              #directory to store files
+BUCKET_PATH = os.path.join(CURRENT_DIRECTORY, BUCKET_NAME)
+
+app = Flask(__name__)
+app.config['BUCKET_PATH'] = BUCKET_PATH
+
+
+@app.route('/')
+def main():
+
+    return render_template('index.html')
+
+
+@app.route('/temp_folder/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(BUCKET_PATH, filename)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        files = request.files['file']
+        if files:
+            out_data = files.read()
+            filename = files.filename
+            path = os.path.join(BUCKET_PATH, filename)
+
+            file_handle = open(path, "w")
+            #write data to file
+            file_handle.write(out_data)
+            file_handle.close()
+
+            return render_template("editor.html", output = out_data)
+        else:
+            return redirect('/')
+
+
+@app.route('/editor', methods=['GET', 'POST'])
+def edit_file():
+    if request.method == 'POST':
+        files = request.form["file"]
+        filename = files.filename
+        path = os.path.join(BUCKET_PATH, filename)
+        file_handle = open(path, "r")
+
+    return render_template("editor.html", output = file_handle.read() )
+
+
+@app.route("/", methods=['POST'])
+def text2File():
+
+    path = os.path.join(BUCKET_PATH, "test.pml")
+    text = request.form["text"]
+    file_handle = open(path, "w")
+    #save input file to local temp file
+    file_handle.write(text)
+    file_handle.close()
+
+
+    process = subprocess.Popen(["peos/pml/check/pmlcheck",path],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    #except OSError as error:
+    #    return error
+
+    output_res, err = process.communicate()
+    if process.returncode > 0:
+        return err
+    return render_template('result.html', output = output_res)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
